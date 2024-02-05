52dd182f6ef575665e0eb46ae6b3d0264714e924
Hamza <hamzasalarian@gmail.com>
2024-02-05 09:09:55 +0500

    Version 2

--- app/src/main/java/com/project/taskmate/LoginActivity.java
+++ app/src/main/java/com/project/taskmate/LoginActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView register;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,19 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(LoginActivity.this, Signup.class);
+                startActivity(i);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, MainScreen.class);
                 startActivity(i);
             }
         });
